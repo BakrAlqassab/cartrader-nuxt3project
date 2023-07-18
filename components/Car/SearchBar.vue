f9c685090dfc68c2eace2f0466e52c4d19999031
bakr <alqassab31@gmail.com>
2023-07-18 16:09:26 +0300

    create components and start in routing

---
+++ components/Car/SearchBar.vue
@@ -0,0 +1,22 @@
+<template >
+  <div>
+    <div>
+      <div
+        class="font-serif w-[1000px] text-2xl rounded-full bg-white flex justify-between overflow-hidden drop-shadow-2xl mx-auto">
+        <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
+          placeholder="Search by city..." v-model="city" />
+        <button class="bg-sky-500 px-10 text-white" @click="handleSearch">Search</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+const city = ref("");
+const router = useRouter()
+
+function handleSearch() {
+
+  router.push(`city/${city.value}/car`)
+}
+
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <!-- for some reason not working -->
+    <header>
+        <NavBar />
+    </header>
+    <div class="container mx-auto p-4">
+        <slot />
+    </div>
+    <Footer>
+        Footer
+
+    </Footer>
+</template>
